5bfb069db86a092ce39bb2779a81d0b4fad74696
anshsahu-01 <anshsahu178@gmail.com>
2026-06-26 14:06:42 +0530

    Problem Add Two Number By Taking Input Solved

--- src/Scanner_Input.java
+++ src/Scanner_Input.java
@@ -4,7 +4,7 @@ public class Scanner_Input {
     public static void main(String[] args){
         System.out.println("Enter the integer value : ");
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println(a);
+        long l = sc.nextLong();
+        System.out.println("Long Value"+ l);
     }
 }
